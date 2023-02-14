3b27f9e03f3dfd315d8a151bd0edbeef0c955f3a
mont266 <scottmontford@gmail.com>
2023-02-14 20:26:35 +0000

    Update 19.8
    -Banksman training promo video added
    -8 New pricing tarrifs added to calc

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,4 +1,4 @@
-//Full Website
+    //Full Website
 function calculate() {
 
     var websiteCost = 250.0;
@@ -44,7 +44,13 @@ function calculate() {
             document.getElementById("deposit").innerHTML = "";
             document.getElementById("monthlyPayment").innerHTML = "";
         }
-        if (document.getElementById("maintain").value == "101-250") {
+        if (document.getElementById("maintain").value == "101-150") {
+            totalCost = 5400 * discount;
+            costPerEmployee = (totalCost / 12) / 150;
+            document.getElementById("deposit").innerHTML = "";
+            document.getElementById("monthlyPayment").innerHTML = "";
+        }
+        if (document.getElementById("maintain").value == "151-250") {
             totalCost = 9600 * discount;
             costPerEmployee = (totalCost / 12) / 250;
             document.getElementById("deposit").innerHTML = "";
@@ -62,19 +68,61 @@ function calculate() {
             document.getElementById("deposit").innerHTML = "";
             document.getElementById("monthlyPayment").innerHTML = "";
         }
-        if (document.getElementById("maintain").value == "751-1000") {
+        if (document.getElementById("maintain").value == "751-875") {
+            totalCost = 21000 * discount;
+            costPerEmployee = (totalCost / 12) / 875;
+            document.getElementById("deposit").innerHTML = "";
+            document.getElementById("monthlyPayment").innerHTML = "";
+        }
+        if (document.getElementById("maintain").value == "876-1000") {
             totalCost = 22800 * discount;
             costPerEmployee = (totalCost / 12) / 1000;
             document.getElementById("deposit").innerHTML = "";
             document.getElementById("monthlyPayment").innerHTML = "";
         }
-        if (document.getElementById("maintain").value == "1001-2500") {
+        if (document.getElementById("maintain").value == "1001-1500") {
+            totalCost = 26800 * discount;
+            costPerEmployee = (totalCost / 12) / 1500;
+            document.getElementById("deposit").innerHTML = "";
+            document.getElementById("monthlyPayment").innerHTML = "";
+        }
+        if (document.getElementById("maintain").value == "1501-2000") {
+            totalCost = 32000 * discount;
+            costPerEmployee = (totalCost / 12) / 2000;
+            document.getElementById("deposit").innerHTML = "";
+            document.getElementById("monthlyPayment").innerHTML = "";
+        }
+        if (document.getElementById("maintain").value == "2001-2500") {
             totalCost = 40800 * discount;
             costPerEmployee = (totalCost / 12) / 2500;
             document.getElementById("deposit").innerHTML = "";
             document.getElementById("monthlyPayment").innerHTML = "";
         }
-        if (document.getElementById("maintain").value == "2501-5000") {
+        if (document.getElementById("maintain").value == "2501-3000") {
+            totalCost = 44000 * discount;
+            costPerEmployee = (totalCost / 12) / 3000;
+            document.getElementById("deposit").innerHTML = "";
+            document.getElementById("monthlyPayment").innerHTML = "";
+        }
+        if (document.getElementById("maintain").value == "3001-3500") {
+            totalCost = 46000 * discount;
+            costPerEmployee = (totalCost / 12) / 3500;
+            document.getElementById("deposit").innerHTML = "";
+            document.getElementById("monthlyPayment").innerHTML = "";
+        }
+        if (document.getElementById("maintain").value == "3501-4000") {
+            totalCost = 48000 * discount;
+            costPerEmployee = (totalCost / 12) / 4000;
+            document.getElementById("deposit").innerHTML = "";
+            document.getElementById("monthlyPayment").innerHTML = "";
+        }
+        if (document.getElementById("maintain").value == "4001-4500") {
+            totalCost = 51000 * discount;
+            costPerEmployee = (totalCost / 12) / 4500;
+            document.getElementById("deposit").innerHTML = "";
+            document.getElementById("monthlyPayment").innerHTML = "";
+        }
+        if (document.getElementById("maintain").value == "4501-5000") {
             totalCost = 54000 * discount;
             costPerEmployee = (totalCost / 12) / 5000;
             document.getElementById("deposit").innerHTML = "";
@@ -114,7 +162,15 @@ function calculate() {
             document.getElementById("deposit").innerHTML = "Deposit: £" + deposit;
             document.getElementById("monthlyPayment").innerHTML = "11 Monthly Installments Of: £" + (Math.round(monthlyPayment * 100) / 100).toFixed(2);;
         }
-        if (document.getElementById("maintain").value == "101-250") {
+        if (document.getElementById("maintain").value == "101-150") {
+            totalCost = 7200;
+            costPerEmployee = (totalCost / 12) / 150;
+            deposit = totalCost / 2;
+            monthlyPayment = deposit / 11;
+            document.getElementById("deposit").innerHTML = "Deposit: £" + deposit;
+            document.getElementById("monthlyPayment").innerHTML = "11 Monthly Installments Of: £" + (Math.round(monthlyPayment * 100) / 100).toFixed(2);;
+        }
+        if (document.getElementById("maintain").value == "151-250") {
             totalCost = 9600;
             costPerEmployee = (totalCost / 12) / 250;
             deposit = totalCost / 2;
@@ -138,7 +194,15 @@ function calculate() {
             document.getElementById("deposit").innerHTML = "Deposit: £" + deposit;
             document.getElementById("monthlyPayment").innerHTML = "11 Monthly Installments Of: £" + (Math.round(monthlyPayment * 100) / 100).toFixed(2);;
         }
-        if (document.getElementById("maintain").value == "751-1000") {
+        if (document.getElementById("maintain").value == "751-875") {
+            totalCost = 21000;
+            costPerEmployee = (totalCost / 12) / 875;
+            deposit = totalCost / 2;
+            monthlyPayment = deposit / 11;
+            document.getElementById("deposit").innerHTML = "Deposit: £" + deposit;
+            document.getElementById("monthlyPayment").innerHTML = "11 Monthly Installments Of: £" + (Math.round(monthlyPayment * 100) / 100).toFixed(2);;
+        }
+        if (document.getElementById("maintain").value == "876-1000") {
             totalCost = 22800;
             costPerEmployee = (totalCost / 12) / 1000;
             deposit = totalCost / 2;
@@ -146,7 +210,23 @@ function calculate() {
             document.getElementById("deposit").innerHTML = "Deposit: £" + deposit;
             document.getElementById("monthlyPayment").innerHTML = "11 Monthly Installments Of: £" + (Math.round(monthlyPayment * 100) / 100).toFixed(2);;
         }
-        if (document.getElementById("maintain").value == "1001-2500") {
+        if (document.getElementById("maintain").value == "1001-1500") {
+            totalCost = 26800;
+            costPerEmployee = (totalCost / 12) / 1500;
+            deposit = totalCost / 2;
+            monthlyPayment = deposit / 11;
+            document.getElementById("deposit").innerHTML = "Deposit: £" + deposit;
+            document.getElementById("monthlyPayment").innerHTML = "11 Monthly Installments Of: £" + (Math.round(monthlyPayment * 100) / 100).toFixed(2);;
+        }
+        if (document.getElementById("maintain").value == "1501-2000") {
+            totalCost = 32000;
+            costPerEmployee = (totalCost / 12) / 2000;
+            deposit = totalCost / 2;
+            monthlyPayment = deposit / 11;
+            document.getElementById("deposit").innerHTML = "Deposit: £" + deposit;
+            document.getElementById("monthlyPayment").innerHTML = "11 Monthly Installments Of: £" + (Math.round(monthlyPayment * 100) / 100).toFixed(2);;
+        }
+        if (document.getElementById("maintain").value == "2001-2500") {
             totalCost = 40800;
             costPerEmployee = (totalCost / 12) / 2500;
             deposit = totalCost / 2;
@@ -154,7 +234,39 @@ function calculate() {
             document.getElementById("deposit").innerHTML = "Deposit: £" + deposit;
             document.getElementById("monthlyPayment").innerHTML = "11 Monthly Installments Of: £" + (Math.round(monthlyPayment * 100) / 100).toFixed(2);;
         }
-        if (document.getElementById("maintain").value == "2501-5000") {
+        if (document.getElementById("maintain").value == "2501-3000") {
+            totalCost = 44000;
+            costPerEmployee = (totalCost / 12) / 3000;
+            deposit = totalCost / 2;
+            monthlyPayment = deposit / 11;
+            document.getElementById("deposit").innerHTML = "Deposit: £" + deposit;
+            document.getElementById("monthlyPayment").innerHTML = "11 Monthly Installments Of: £" + (Math.round(monthlyPayment * 100) / 100).toFixed(2);;
+        }
+        if (document.getElementById("maintain").value == "3001-3500") {
+            totalCost = 46000;
+            costPerEmployee = (totalCost / 12) / 3500;
+            deposit = totalCost / 2;
+            monthlyPayment = deposit / 11;
+            document.getElementById("deposit").innerHTML = "Deposit: £" + deposit;
+            document.getElementById("monthlyPayment").innerHTML = "11 Monthly Installments Of: £" + (Math.round(monthlyPayment * 100) / 100).toFixed(2);;
+        }
+        if (document.getElementById("maintain").value == "3501-4000") {
+            totalCost = 48000;
+            costPerEmployee = (totalCost / 12) / 4000;
+            deposit = totalCost / 2;
+            monthlyPayment = deposit / 11;
+            document.getElementById("deposit").innerHTML = "Deposit: £" + deposit;
+            document.getElementById("monthlyPayment").innerHTML = "11 Monthly Installments Of: £" + (Math.round(monthlyPayment * 100) / 100).toFixed(2);;
+        }
+        if (document.getElementById("maintain").value == "4001-4500") {
+            totalCost = 51000;
+            costPerEmployee = (totalCost / 12) / 4500;
+            deposit = totalCost / 2;
+            monthlyPayment = deposit / 11;
+            document.getElementById("deposit").innerHTML = "Deposit: £" + deposit;
+            document.getElementById("monthlyPayment").innerHTML = "11 Monthly Installments Of: £" + (Math.round(monthlyPayment * 100) / 100).toFixed(2);;
+        }
+        if (document.getElementById("maintain").value == "4501-5000") {
             totalCost = 54000;
             costPerEmployee = (totalCost / 12) / 5000;
             deposit = totalCost / 2;
